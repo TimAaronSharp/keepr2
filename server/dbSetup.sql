f35de30e7826df48dd2b54807d2565e592016f0e
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-06-04 13:40:04 -0600

    CREATED: |database| - keeps, vaults, vault_keeps mySQL TABLES, keep_count VIEW

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,51 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name VARCHAR(255) COMMENT 'User Name',
-  email VARCHAR(255) UNIQUE COMMENT 'User Email',
-  picture VARCHAR(255) COMMENT 'User Picture'
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) COMMENT 'User Name',
+    email VARCHAR(255) UNIQUE COMMENT 'User Email',
+    picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE keeps (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    img VARCHAR(1000) NOT NULL,
+    views INT NOT NULL DEFAULT 0,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE vaults (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    img VARCHAR(1000) NOT NULL,
+    is_private BOOLEAN NOT NULL DEFAULT false,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE TABLE vault_keeps (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    keep_id INT NOT NULL,
+    vault_id INT NOT NULL,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (keep_id) REFERENCES keeps (id) ON DELETE CASCADE,
+    FOREIGN KEY (vault_id) REFERENCES vaults (id) ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+CREATE VIEW keep_count AS
+SELECT keeps.*, COUNT(vault_keeps.keep_id) AS kept
+FROM keeps
+    LEFT OUTER JOIN vault_keeps ON vault_keeps.keep_id = keeps.id
+GROUP BY
+    keeps.id
