d313c8d8774387a0dbeb22526dbe1e7b9d53830f
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-06-10 11:16:41 -0600

    CREATED: |client| - KeepCard.vue
    ADDED: |client| - AppState.keeps[], structure in HomePage.vue
    
    REMOVED: |client| - Example.vue

---
+++ client/src/components/KeepCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Keep } from '@/models/Keep.js';
+
+
+
+const props = defineProps({
+  keepProp: { type: Keep, required: true }
+})
+
+
+</script>
+
+
+<template>
+  <div>
+    <img :src="keepProp?.img" :alt="`A picture for the ${keepProp?.name} keep by ${keepProp.creator?.name}`"
+      :title="`A picture for the keep titled ${keepProp.name}`">
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,25 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+import { AppState } from '@/AppState.js';
+import KeepCard from '@/components/KeepCard.vue';
+import { computed } from 'vue';
 
+
+const keeps = computed(() => AppState.keeps)
 
 </script>
 
 <template>
-  <Example />
+  <section class="container">
+    <div class="row">
+      <div class="col-12">
+        <div>
+          <div v-for="keep in keeps" :key="'keep ' + keep.id">
+            <KeepCard :keepProp="keep" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
