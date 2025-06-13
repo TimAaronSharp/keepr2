6cd3e8a6a702638a363a2112d9ec67c02b1ceb2f
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-06-13 16:11:52 -0600

    CREATED: AccountController - GetMyVaults()

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -5,15 +5,20 @@ namespace keepr2.Controllers;
 [Route("[controller]")]
 public class AccountController : ControllerBase
 {
+  // NOTE 💉 Dependency injections.
   private readonly AccountService _accountService;
   private readonly Auth0Provider _auth0Provider;
+  private readonly VaultsService _vaultsService;
 
-  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  // NOTE 🏗️ Class constructor.
+  public AccountController(AccountService accountService, Auth0Provider auth0Provider, VaultsService vaultsService)
   {
     _accountService = accountService;
     _auth0Provider = auth0Provider;
+    _vaultsService = vaultsService;
   }
 
+  // NOTE 🔍📄 Get user account method request path.
   [HttpGet]
   public async Task<ActionResult<Account>> Get()
   {
@@ -25,6 +30,22 @@ public class AccountController : ControllerBase
     catch (Exception e)
     {
       return BadRequest(e.Message);
+    }
+  }
+
+  // NOTE 🔍🏦📄 Get user vaults by account id method request path.
+  [Authorize]
+  [HttpGet("vaults")]
+  public async Task<ActionResult<Vault>> GetMyVaults()
+  {
+    try
+    {
+      Profile userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_vaultsService.GetByProfileId(userInfo.Id, userInfo));
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
     }
   }
 }
