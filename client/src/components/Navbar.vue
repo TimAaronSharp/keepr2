454c976ad41cb2642c1d2d675ca97301962a4d04
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-07-03 21:14:41 -0600

    REFACTORED: KeepCard.vue - Moved .margin-left-auto{} CSS rule to style.scss so it can be used across entire app. Navbar.vue - Replaced Create Keep/Vault buttons with dropdown menu that opens appropriate create modal on click.
    ADDED: style.scss - .transparent-btn-style{}

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -25,38 +25,50 @@ watch(theme, () => {
         <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" />
         <b class="fs-5">Vue Starter</b>
       </RouterLink>
+      <div v-if="account" class="d-flex align-items-center text-light dropdown">
+        <button role="button" class="transparent-btn-style text-light" data-bs-toggle="dropdown" aria-expanded="false"
+          title="Open Create Menu">
+          Create
+          <span class="mdi mdi-arrow-down-drop-circle-outline"></span>
+        </button>
+        <div class="dropdown-menu dropdown-menu-start p-0" role="menu">
+          <div class="list-group">
+            <button data-bs-toggle="modal" data-bs-target="#create-keep-modal"
+              class="list-group-item dropdown-item list-group-item-action selectable" title="Create New Keep">
+              Create Keep</button>
+            <button data-bs-toggle="modal" data-bs-target="#create-vault-modal"
+              class="list-group-item dropdown-item list-group-item-action selectable" title="Create New Vault">
+              Create Vault</button>
+          </div>
+        </div>
+      </div>
+      <!-- NOTE Will revisit if I decide to implement anything that a collapsable button would be useful for -->
       <!-- collapse button -->
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
+      <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
         aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="mdi mdi-menu text-light"></span>
-      </button>
+      </button> -->
       <!-- collapsing menu -->
-      <div class="collapse navbar-collapse " id="navbar-links">
+      <!-- <div class="collapse navbar-collapse " id="navbar-links">
         <ul class="navbar-nav">
           <li>
             <RouterLink :to="{ name: 'About' }" class="btn text-green selectable">
               About
             </RouterLink>
           </li>
-        </ul>
-        <!-- NOTE Figure out how to make modals work with select list. -->
-        <div v-if="account">
-          <button type="button" class="btn btn-primary ms-2" data-bs-toggle="modal"
-            data-bs-target="#create-keep-modal">Create Keep</button>
-          <button type="button" class="btn btn-primary ms-2" data-bs-toggle="modal"
-            data-bs-target="#create-vault-modal">Create Vault</button>
-        </div>
-        <!-- LOGIN COMPONENT HERE -->
-        <div class="ms-auto">
-          <button class="btn text-light" @click="toggleTheme"
+        </ul> -->
+      <!-- LOGIN COMPONENT HERE -->
+      <!-- NOTE Will revisit integrating theme toggling later -->
+      <!-- <div class="ms-auto"> -->
+      <!-- <button class="btn text-light" @click="toggleTheme"
             :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
             <i v-if="theme == 'dark'" class="mdi mdi-weather-sunny"></i>
             <i v-if="theme == 'light'" class="mdi mdi-weather-night"></i>
-          </button>
-        </div>
-        <Login />
-      </div>
+          </button> -->
+      <!-- </div> -->
+      <Login class="margin-left-auto" />
     </div>
+    <!-- </div> -->
   </nav>
 </template>
 
