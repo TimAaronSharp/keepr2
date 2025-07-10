f3930734426746215cd1c5f09920ee4331e374e9
TimAaronSharp <32853687+TimAaronSharp@users.noreply.github.com>
2025-07-10 03:02:22 -0600

    Update env.js
    Testing removing enj.js variables again and using secrets

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,1 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
-export const useSockets = false
-export const domain = 'dev-tujbwv7x02il14en.us.auth0.com'
-export const clientId = 'obtpms2liE9zifUNOMhOQUMokbv50IzI'
-export const audience = 'https://auth.speedwagonfoundation.com'
+
